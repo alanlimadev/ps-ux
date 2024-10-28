ae4560bf44cb3115ec429540bb650171108c6c3c
Alan Lima <alanreislm@gmail.com>
2024-10-28 19:29:37 -0300

    refactor(order): add constructor to Order entity for default values

--- api-delivery-agil/src/orders/order.entity.ts
+++ api-delivery-agil/src/orders/order.entity.ts
@@ -4,4 +4,18 @@ export class Order {
   products: string[];
   address: string;
   status: 'Pendente' | 'Em Andamento' | 'Concluído';
+
+  constructor(
+    id: number,
+    name: string,
+    products: string[] = [],
+    address: string = '',
+    status: 'Pendente' | 'Em Andamento' | 'Concluído' = 'Pendente',
+  ) {
+    this.id = id;
+    this.name = name;
+    this.products = products;
+    this.address = address;
+    this.status = status;
+  }
 }
